8f11fd2b4a5051b36c277e1ed16eae3629b3a5cf
sarveenyadav7 <sarveenyadav7@gmail.com>
2022-12-21 11:23:50 +0530

    wises update

--- script.js
+++ script.js
@@ -6,14 +6,28 @@ function getTime(){
     let presentMinutes = presentTime.getMinutes()
     let presentSeconds = presentTime.getSeconds()
 
-
-    if(presentHours <12){
-        hours = hours - 12
-        document.querySelector('#present_houram-pm').innerText = 'A.M'
+    if(presentHours <=12){
+        document.querySelector('#present_houram-pm').innerText = 'Good Morning'
+    }
+    else if(presentHours <=16){
+        document.querySelector('#present_houram-pm').innerText = 'Good Afternoon'
+    }
+    else if(presentHours <=20){
+        document.querySelector('#present_houram-pm').innerText = 'Good Evening'
     }
     else{
-        document.querySelector('#present_houram-pm').innerText = 'P.M'
-    }
+        document.querySelector('#present_houram-pm').innerText = 'Good Night'
+    } 
+
+
+
+    // if(presentHours <12){
+    //     hours = hours - 12
+    //     document.querySelector('#present_houram-pm').innerText = 'A.M'
+    // }
+    // else{
+    //     document.querySelector('#present_houram-pm').innerText = 'P.M'
+    // }
 
 
 
@@ -23,11 +37,11 @@ function getTime(){
 
 
     if(presentHours < 10){
-        hours = '0' + hours
+        present_hours = '0' + hours
     } else if (presentMinutes < 10){
-        minutes = '0' + minutes
+        present_minutes = '0' + minutes
     } else if (presentSeconds < 10){
-        seconds = '0' + seconds
+        // present_seconds = '0' + seconds
     }
 
 
